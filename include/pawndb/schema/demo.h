1d0116c48f3c64d31251577adfa0515efc2585ab
Xiahua Liu <49677912+xiahualiu@users.noreply.github.com>
2025-01-27 04:02:24 +0000

    Update

--- include/pawndb/schema/demo.h
+++ include/pawndb/schema/demo.h
@@ -1,14 +1,3 @@
-/**
- * @file demo.h
- * @author Xiahua Liu @xiahualiu
- * @brief PawnDB demo schema.
- * @version 0.1
- * @date 2025-01-02
- *
- * @copyright MIT License
- *
- */
-
 #ifndef PAWNDB_SCHEMA_DEMO_H
 #define PAWNDB_SCHEMA_DEMO_H
 
